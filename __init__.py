f4c54384fedfb7fb43612cddc3bec1b323f912d4
Daniel Zanco <dangpzanco@gmail.com>
2021-01-06 23:43:23 -0300

    add init file for easier module integration

---
+++ __init__.py
@@ -0,0 +1,10 @@
+from .aircraft import Aircraft
+from .cars import Cars
+from .cub2011 import Cub2011
+from .dogs import Dogs
+from .inat2017 import INat2017
+from .nabirds import NABirds
+from .tiny_imagenet import TinyImageNet
+
+__all__ = ['Aircraft', 'Cars', 'Cub2011', 'Dogs',
+           'INat2017', 'NABirds', 'TinyImageNet', 'StanfordDogs']
